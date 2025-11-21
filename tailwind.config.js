b513cc3321186b0be7a4dbce00fc5610449c8808
xexu29 <ratulicleme@gmail.com>
2025-11-21 10:18:38 +0100

    el comienzo del fin

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./assets/*/.{js,ts,jsx,tsx}",
+    "./templates/*/.html.twig",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("daisyui")
+  ],
+}
