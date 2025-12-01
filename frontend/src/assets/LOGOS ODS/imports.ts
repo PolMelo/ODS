65878e2badd63fcab404c5999a834befd0bc04e1
2812pol <pol.melo14@gmail.com>
2025-12-01 10:19:52 +0100

    Resources page css, arreglar

---
+++ frontend/src/assets/LOGOS ODS/imports.ts
@@ -0,0 +1,23 @@
+import ODS1 from "./ODS 1.jfif";
+import ODS2 from "./ODS 2.jpg";
+import ODS3 from "./ODS 3.jpg";
+import ODS4 from "./ODS 4.jpg";
+import ODS5 from "./ODS 5.jpg";
+import ODS6 from "./ODS 6.jpg";
+import ODS7 from "./ODS 7.jpg";
+import ODS8 from "./ODS 8.jpg";
+import ODS9 from "./ODS 9.jpg";
+import ODS10 from "./ODS 10.jpg";
+import ODS11 from "./ODS 11.jpg";
+import ODS12 from "./ODS 12.jpg";
+import ODS13 from "./ODS 13.jpg";
+import ODS14 from "./ODS 14.jpg";
+import ODS15 from "./ODS 15.jpg";
+import ODS16 from "./ODS 16.jpg";
+import ODS17 from "./ODS 17.jpg";
+
+export {
+  ODS1, ODS2, ODS3, ODS4, ODS5, ODS6, ODS7, ODS8,
+  ODS9, ODS10, ODS11, ODS12, ODS13, ODS14, ODS15,
+  ODS16, ODS17
+};
